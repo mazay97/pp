b1c7f4d11dab9270579dcb54982e8d871a5efec8
Maxim-Zaykov <delcollapse@mail.ru>
2018-05-13 14:47:57 +0300

    Добавлены новые классы

--- lw2/Zaykov Maxim/War/War/War.cpp
+++ lw2/Zaykov Maxim/War/War/War.cpp
@@ -1,11 +1,42 @@
-// War.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
+#include "Reader.h"
+#include "ThreadHandler.h"
+#include "Conflict.h"
 
+const int NUM_OF_THREADS = 2;
+
+struct ConflictData
+{
+	Conflict * m_conflict;
+};
+
+DWORD WINAPI makeShoot(LPVOID * params)
+{
+	while(true) {
+		if (!((ConflictData *)params)->m_conflict->Attack()) {
+			break;
+		}
+	}
+	return 0;
+}
 
 int main()
 {
-    return 0;
-}
+	Reader reader("input.txt");
+	CState state1(reader.ParseState());
+	CState state2(reader.ParseState());
+	Conflict conflict(state1, state2);
+	
+	ConflictData conflictData;
+	conflictData.m_conflict = &conflict;
 
+	ThreadHandler handler;
+	handler.AddThreads((LPTHREAD_START_ROUTINE)makeShoot, (LPVOID)&conflictData, 5);
+	handler.RunThreads();
+	handler.CloseThreads();
+
+	conflictData.m_conflict->m_firstState.GetTerritory()->PrintTerritory();
+	conflictData.m_conflict->m_secondState.GetTerritory()->PrintTerritory();
+
+	return 0;
+}
--- lw2/Zaykov Maxim/War/War/stdafx.h
+++ lw2/Zaykov Maxim/War/War/stdafx.h
@@ -1,15 +1,13 @@
-// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
-// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
-// не часто измен€ютс€
-//
-
 #pragma once
 
 #include "targetver.h"
 
 #include <stdio.h>
 #include <tchar.h>
-
-
-
-// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <random>
+#include <windows.h>
